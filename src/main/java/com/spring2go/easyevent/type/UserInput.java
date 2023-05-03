0d92afc17f2279a8e190c4c19379403c5ec68367
mianmiantea2019 <sguo321@gatech.edu>
2023-05-02 23:25:45 -0400

    update userinput

---
+++ src/main/java/com/spring2go/easyevent/type/UserInput.java
@@ -0,0 +1,16 @@
+package com.spring2go.easyevent.type;
+
+/**
+ * @author: Christy Guo
+ * @create_date: 2023-05-02 11:25 PM
+ * @desc:
+ * @modifier:
+ */
+
+import lombok.Data;
+
+@Data
+public class UserInput {
+    private String email;
+    private String password;
+}
