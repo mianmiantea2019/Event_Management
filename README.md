216626b53f32ece4ab50065ee4c023c5c9c3496a
mianmiantea2019 <sguo321@gatech.edu>
2023-05-03 09:39:20 -0400

    fix context

--- README.md
+++
@@ -1,4 +0,0 @@
-# Event_Management
-Event_Management
-
-![alt text](image/Domain_Model.JPG)
--- src/main/java/com/spring2go/easyevent/custom/AuthContext.java
+++ src/main/java/com/spring2go/easyevent/custom/AuthContext.java
@@ -16,7 +16,7 @@ public class AuthContext {
     private boolean tokenInvalid;
 
     public void ensureAuthenticated() {
-        if (tokenInvalid) throw new RuntimeException("令牌无效！");
-        if (userEntity == null) throw new RuntimeException("未登陆，请先登陆！");
+        if (tokenInvalid) throw new RuntimeException("invalid token！");
+        if (userEntity == null) throw new RuntimeException("please login！");
     }
 }
