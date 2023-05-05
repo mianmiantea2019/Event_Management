2ad1269f2db0c18543f3c680c71864043094658d
mianmiantea2019 <sguo321@gatech.edu>
2023-05-05 00:04:14 -0400

    update booking function

--- src/main/java/com/spring2go/easyevent/fetcher/BookingDataFetcher.java
+++ src/main/java/com/spring2go/easyevent/fetcher/BookingDataFetcher.java
@@ -11,5 +11,48 @@
 @DgsComponent
 @RequiredArgsConstructor
 public class BookingDataFetcher {
+  private final BookingEntityMapper bookingEntityMapper;
+    private final EventEntityMapper eventEntityMapper;
+    private final UserEntityMapper userEntityMapper;
 
+    @DgsQuery
+    public List<Booking> bookings(DataFetchingEnvironment dfe) {
+        AuthContext authContext = DgsContext.getCustomContext(dfe);
+        authContext.ensureAuthenticated();
+
+        QueryWrapper<BookingEntity> queryWrapper = new QueryWrapper<>();
+        queryWrapper.lambda().eq(BookingEntity::getUserId, authContext.getUserEntity().getId());
+        List<Booking> bookings = bookingEntityMapper.selectList(queryWrapper)
+                .stream()
+                .map(Booking::fromEntity)
+                .collect(Collectors.toList());
+        return bookings;
+    }
+
+    @DgsMutation
+    public Event cancelBooking(@InputArgument(name = "bookingId") String bookingIdString,
+                               DataFetchingEnvironment dfe) {
+        AuthContext authContext = DgsContext.getCustomContext(dfe);
+        authContext.ensureAuthenticated();
+
+        Integer bookingId = Integer.parseInt(bookingIdString);
+
+        BookingEntity bookingEntity = bookingEntityMapper.selectById(bookingId);
+        if (bookingEntity == null) {
+            throw new RuntimeException(String.format("Booking with id %s does not exist", bookingIdString));
+        }
+
+        Integer userId = bookingEntity.getUserId();
+        UserEntity userEntity = authContext.getUserEntity();
+        if (!userEntity.getId().equals(userId)) {
+            throw new RuntimeException("You are not allowed to cancel other people's booking!");
+        }
+
+        bookingEntityMapper.deleteById(bookingId);
+
+        Integer eventId = bookingEntity.getEventId();
+        EventEntity eventEntity = eventEntityMapper.selectById(eventId);
+        Event event = Event.fromEntity(eventEntity);
+        return event;
+    }
 }
