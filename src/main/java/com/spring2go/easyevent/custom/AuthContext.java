a367d20048d019c3ce6557be34f4709955d2eba5
mianmiantea2019 <sguo321@gatech.edu>
2023-05-05 00:09:34 -0400

    update authcontext

--- src/main/java/com/spring2go/easyevent/custom/AuthContext.java
+++ src/main/java/com/spring2go/easyevent/custom/AuthContext.java
@@ -7,5 +7,17 @@ package com.spring2go.easyevent.custom;
  * @modifier:
  */
 
-public class AuthContext {
-}
+ import com.spring2go.easyevent.entity.UserEntity;
+ import lombok.Data;
+ 
+ @Data
+ public class AuthContext {
+     private UserEntity userEntity;
+     private boolean tokenInvalid;
+ 
+     public void ensureAuthenticated() {
+         if (tokenInvalid) throw new RuntimeException("invalid token！");
+         if (userEntity == null) throw new RuntimeException("please login in！");
+     }
+ }
+ 
