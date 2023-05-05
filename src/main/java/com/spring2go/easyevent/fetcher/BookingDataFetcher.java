a627919a0762a2b1dc6b995f20c9531ed610ff22
mianmiantea2019 <sguo321@gatech.edu>
2023-05-05 00:02:46 -0400

    create booking data fetcher

---
+++ src/main/java/com/spring2go/easyevent/fetcher/BookingDataFetcher.java
@@ -0,0 +1,15 @@
+/*
+ * @Author: mianmiantea2019
+ * @Date: 2023-05-04 23:52:58
+ * @LastEditors: mianmiantea2019
+ * @LastEditTime: 2023-05-04 23:53:01
+ * @Description: 
+ */
+
+
+@Slf4j
+@DgsComponent
+@RequiredArgsConstructor
+public class BookingDataFetcher {
+
+}
